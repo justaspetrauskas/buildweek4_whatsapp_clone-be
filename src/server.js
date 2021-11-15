af21760a176680b9a667ac33a137958993879f91
js25s <petrauskas.justas@gmail.com>
2021-11-15 11:46:54 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,29 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
+import listEndpoints from "express-list-endpoints";
+
+const server = express();
+
+const { PORT = 3009 } = process.env;
+
+// MIDDLEWARE
+
+server.use(cors());
+server.use(express.json());
+
+// SERVICES
+
+server.listen(PORT, () => {
+  // connect to mongoose Server
+
+  mongoose.connect(process.env.MONGODB, {});
+
+  console.log(`Server is listening on port ${PORT}`);
+  console.table(listEndpoints(server));
+});
+
+server.on("error", (error) => {
+  console.log("Server is stopped ", error);
+});
