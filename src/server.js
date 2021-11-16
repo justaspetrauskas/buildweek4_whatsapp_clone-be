84bd10090aa1be3d8b3a52a8de641569924a38c1
js25s <petrauskas.justas@gmail.com>
2021-11-16 01:19:02 +0100

    to be continued

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import listEndpoints from "express-list-endpoints";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 import ChatRouter from "./services/chats/index.js";
 
@@ -16,6 +18,27 @@ server.use(express.json());
 
 // SERVICES
 server.use("/chats", ChatRouter);
+
+const httpServer = createServer(server);
+
+const io = new Server(httpServer, { allowEIO3: true });
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("send_message", async ({ message, members }) => {
+    await ChatModel.findOneAndUpdate(
+      { members },
+      { $push: { chatHistory: message } }
+    );
+
+    socket.to(members).emit("receive_message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
 // SERVER
 server.listen(PORT, () => {
--- src/services/chats/index.js
+++ src/services/chats/index.js
@@ -6,12 +6,12 @@ const chatRouter = express.Router();
 chatRouter.get("/", async (req, res, next) => {
   try {
     //   Returns all chats in which you are a member
-    const chats = await ChatModel.find().populate({
+    const chats = await ChatModel.find({ user: req.user }).populate({
       path: "history",
       select: "content",
     });
 
-    res.send(accomodations);
+    res.send(chats);
   } catch (error) {
     next(error);
   }
@@ -19,6 +19,13 @@ chatRouter.get("/", async (req, res, next) => {
 
 chatRouter.post("/", async (req, res, next) => {
   try {
+    // check if the request sender had and active chat with this user (look for the history)
+    const chatExists = await ChatModel.findOne({ members: req.body.userId });
+
+    // if not: create a new chat among the request sender and the members listed in the request body
+    const newChat = new ChatModel({
+      members: [req.body.receiverId],
+    });
   } catch (error) {
     next(error);
   }
@@ -26,6 +33,8 @@ chatRouter.post("/", async (req, res, next) => {
 
 chatRouter.get("/:chatID", async (req, res, next) => {
   try {
+    // must be a chat member
+    const chat = await ChatModel.findOne({ history: req.params.chatID });
   } catch (error) {
     next(error);
   }
