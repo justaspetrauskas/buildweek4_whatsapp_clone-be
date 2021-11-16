ce30e764a3e0cfd81dfea68a5a56e996ea3bf983
js25s <petrauskas.justas@gmail.com>
2021-11-16 11:24:23 +0100

    jwt authentication

---
+++ src/Authorization/member.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import ChatModel from "../schemas/chatSchema.js";
+
+export const isChatMember = async (req, res, next) => {
+  try {
+    const chat = await ChatModel.findById(req.params.id);
+    if (chat) {
+      const isMember = chat.members.equals(req.user._id);
+      console.log(isMember);
+      if (isMember) {
+        next();
+      } else {
+        res.status(404).send("not the member of the conversation");
+      }
+    } else {
+      res.status(404, "Chat not found");
+    }
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ src/Authorization/tools.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+
+export const JWTAuthenticate = async (user) => {
+  const accessToken = await generateJWT({ _id: user._id });
+  user.accessToken = accessToken;
+  await user.save();
+  return accessToken;
+};
+
+export const generateJWT = (payload) =>
+  new Promise((resolve, reject) =>
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "1 week" },
+      (err, token) => {
+        if (err) reject(err);
+        resolve(token);
+      }
+    )
+  );
+
+export const verifyJWT = (token) =>
+  new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) reject(err);
+      resolve(decodedToken);
+    });
+  });
